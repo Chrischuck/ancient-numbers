86ccbfa5b2e114a65190510bb8413892890d7c56
Chris <chrischuck35@gmail.com>
2018-02-21 23:11:34 -0800

    Initializes vuex

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
 import App from './App.vue'
 import router from './router.js'
 
-OfflinePluginRuntime.install();
+Vue.use(Vuex)
 
 export default new Vue({
   el: '#app',
   router,
   render: h => h(App)
 })
+
+OfflinePluginRuntime.install();
+
